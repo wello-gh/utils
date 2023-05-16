9a55cab64f5ddf7edd82c0338a19e8a0dcf52ee2
wello <132093701+wello-gh@users.noreply.github.com>
2023-05-16 17:39:00 +0500

    Create debug.lua

---
+++ debug.lua
@@ -0,0 +1,11 @@
+local owner = "Upbolt"
+local branch = "revision"
+
+local function webImport(file)
+    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Hydroxide/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
+end
+
+webImport("init")
+webImport("ui/main")
+
+loadstring(game:HttpGetAsync("https://cdn.wearedevs.net/scripts/Dex%20Explorer.txt"))()
